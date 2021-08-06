67d4dd28725f98fbfb2b130a61527aad0caa1692
tanguyvda <tcharles@centreon.com>
2021-08-06 13:01:02 +0200

    improve sql

--- sql/install.sql
+++ sql/install.sql
@@ -14,22 +14,26 @@ CREATE TABLE `mod_ccvm_custom_view_ownership` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `custom_view_id` int(11) NOT NULL,
   `new_owner` int(11) NOT NULL,
-  `old_owner`int(11) NOT NULL,
+  `old_owner`int(11),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+-- 
+-- unique key
+-- 
+ALTER TABLE mod_ccvm_custom_view_ownership
+ADD CONSTRAINT uk_viewowner UNIQUE (custom_view_id, new_owner);
 -- 
 -- foreign keys
 --
 ALTER TABLE mod_ccvm_custom_view_ownership
 ADD CONSTRAINT fk_customviewid FOREIGN KEY (custom_view_id) REFERENCES custom_views(custom_view_id)
-ON DELETE CASCADE
+ON DELETE CASCADE;
 
 ALTER TABLE mod_ccvm_custom_view_ownership
 ADD CONSTRAINT fk_newowner FOREIGN KEY (new_owner) REFERENCES contact(contact_id)
-ON DELETE CASCADE
+ON DELETE CASCADE;
 
 ALTER TABLE mod_ccvm_custom_view_ownership
 ADD CONSTRAINT fk_oldowner FOREIGN KEY (old_owner) REFERENCES contact(contact_id)
-ON DELETE CASCADE
+ON DELETE SET NULL;
--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -3,3 +3,8 @@
 --
 DELETE FROM topology where `topology_parent`=644;
 DELETE FROM topology where `topology_page`=644;
+
+--
+-- Remove module tables
+--
+DROP TABLE mod_ccvm_custom_view_ownership;
